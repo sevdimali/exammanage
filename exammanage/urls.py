707d030a3568f0e6310528e1a67dbcf6e1d45444
Sevdimali Isayev <github853211>
2021-06-18 00:16:00 +0400

    login page done

--- exammanage/urls.py
+++ exammanage/urls.py
@@ -1,20 +1,5 @@
-"""exammanage URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import Imtahan, DuzgunCavab
 
 from django.conf import settings
@@ -24,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Imtahan.as_view(), name='index'),
     path('dg/<int:pk>', DuzgunCavab.as_view(), name='exam_correct'),
+    path('', include('django.contrib.auth.urls')),
 
 ]
 
