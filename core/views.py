c56a7341af6923e378162188b128b5760f7e8700
Huseyn Huseynzade <huseynhuseynzade345@gmail.com>
2021-06-17 18:13:10 +0400

    exam correct done...

--- core/views.py
+++ core/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Imtahan as imtModel
+from .models import Imtahan as imtModel, ExamCorrect
 
 # Create your views here.
-from django.views.generic.base import ContextMixin,TemplateView
+from django.views.generic.base import ContextMixin, TemplateView
 
 
 class BaseContext(ContextMixin):
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         return context
-
-
-
-def detal(request):
-    return render(request, template_name='exam_correct.html')
 
 
 class Imtahan(BaseContext, TemplateView):
@@ -28,3 +23,15 @@ class Imtahan(BaseContext, TemplateView):
         return context
 
 
+class DuzgunCavab(BaseContext, TemplateView):
+    template_name = 'exam_correct.html'
+    duzgun = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        pk = self.kwargs.get('pk')
+        imtahan = get_object_or_404(imtModel, pk=pk)
+        self.duzgun = ExamCorrect.objects.filter(imtahan_id=imtahan).order_by('imtahan_id')
+        context.update({'duzgun': self.duzgun})
+
+        return context
--- exammanage/urls.py
+++ exammanage/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import Imtahan, detal
+from core.views import Imtahan, DuzgunCavab
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,7 +23,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Imtahan.as_view(), name='index'),
-    path('detal', detal, name='detal'),
+    path('dg/<int:pk>', DuzgunCavab.as_view(), name='exam_correct'),
 
 ]
 
